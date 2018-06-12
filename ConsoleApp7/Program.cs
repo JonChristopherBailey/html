9d5cad0979c831776055b4ac16f91e167ac02e6d
JonChristopherBailey <38774978+JonChristopherBailey@users.noreply.github.com>
2018-06-12 14:51:48 -0500

    Add files via upload

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ConsoleApp7
+{
+    namespace hw_14
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                float number = .5F;
+                string letters = "Enter your age: ";
+                double quantity = 5.5;
+                bool love = true;
+                int s = 4;
+                int a = 5;
+                int d = 6;
+                Console.WriteLine(s);
+                Console.WriteLine(a);
+                Console.WriteLine(d);
+
+                Console.WriteLine(number);
+                Console.WriteLine(letters);
+                Console.WriteLine(quantity);
+                Console.WriteLine(love);
+                Console.ReadKey();
+
+                Console.WriteLine("Pick a number:");
+
+                Console.WriteLine("1 {0} 2 {1} 3 {2}", letters, number, quantity); ;
+                Console.ReadKey();
+              
+        
+
+
+                  
+            }
+               
+          
+                
+
+
+
+
+
+
+
+
+            }
+        }
+    }
